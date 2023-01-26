68eb14cd40ec6225398c19e3891cde484135f3a9
Georgy Kcojev <kcojev@gmail.com>
2023-01-26 15:57:30 +0300

    preloader setup and functionality

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
 const btn = document.querySelector('.switch-btn')
 const video = document.querySelector('.video-container')
@@ -14,4 +11,10 @@ btn.addEventListener('click', function() {
         btn.classList.remove('slide')
         video.play()
     }
+})
+
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', function() {
+    preloader.classList.add('hide-preloader')
 })
